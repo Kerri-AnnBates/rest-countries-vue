537e6f5550513672edf407f2ec01e311aac3a5ca
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-12-11 12:07:31 -0500

    setting up base structure

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    isDarkMode: {
+        type: Boolean
+    }
+})
+</script>
+
+<template>
+    <header>
+        <div className='container'>
+            <div className='flex-header'>
+                <h1>Where in the world?</h1>
+                <button>Dark Mode</button>
+            </div>
+        </div>
+    </header>
+</template>
