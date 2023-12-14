12d8f4c76a63e86d95025effb40d55c999905cfa
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-12-14 11:29:54 -0500

    style header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,3 +16,40 @@ defineProps({
         </div>
     </header>
 </template>
+
+<style lang="scss">
+// Variables
+$white: #fff;
+
+header {
+    box-shadow: 0 7px 10px -9px #4d4d4d;
+    padding: 20px 0;
+
+    button {
+        background-color: $white;
+        padding: 5px;
+        border: none;
+        display: flex;
+        align-items: center;
+        box-shadow: none;
+
+        svg {
+            margin-right: 3px;
+        }
+
+        &:hover {
+            cursor: pointer;
+        }
+
+        @media screen and (min-width: 800px) {
+            padding: 8px;
+        }
+    }
+}
+
+.flex-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
