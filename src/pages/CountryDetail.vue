9d631729a23b7978b060564c200d36114b1c3fcd
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-12-27 11:53:13 -0500

    add container style

--- src/pages/CountryDetail.vue
+++ src/pages/CountryDetail.vue
@@ -1,36 +1,39 @@
 <template>
-    <div className='details-header'>
-        <button className="back">
-            Back
-        </button>
-    </div>
-    <div className="details">
-        <div className="details-img">
-            <img src="https://flagcdn.com/w320/us.png" />
-        </div>
-        <div className="details-content">
-            <h3>United States of America</h3>
-            <div className="content">
-                <section>
-                    <p><span className="highlight">Native Name:</span> United States</p>
-                    <p><span className="highlight">Population:</span> 323,947,000</p>
-                    <p><span className="highlight">Region:</span> Americas</p>
-                    <p><span className="highlight">Sub Region:</span> Northern America</p>
-                    <p><span className="highlight">Capital:</span> Washington D.C</p>
-                </section>
-                <section>
-                    <p><span className="highlight">Top Level Domain:</span> .us</p>
-                    <p><span className="highlight">Currencies:</span> USD</p>
-                    <p><span className="highlight">Languages:</span> en</p>
-                </section>
-            </div>
+    <div class="container">
 
-            <div className="borders">
-                <section>
-                    <h3>Border Countries:</h3>
-                    <!-- List of buttons -->
-                    <button>Mexico</button>
-                </section>
+        <div className='details-header'>
+            <button className="back">
+                Back
+            </button>
+        </div>
+        <div className="details">
+            <div className="details-img">
+                <img src="https://flagcdn.com/w320/us.png" />
+            </div>
+            <div className="details-content">
+                <h3>United States of America</h3>
+                <div className="content">
+                    <section>
+                        <p><span className="highlight">Native Name:</span> United States</p>
+                        <p><span className="highlight">Population:</span> 323,947,000</p>
+                        <p><span className="highlight">Region:</span> Americas</p>
+                        <p><span className="highlight">Sub Region:</span> Northern America</p>
+                        <p><span className="highlight">Capital:</span> Washington D.C</p>
+                    </section>
+                    <section>
+                        <p><span className="highlight">Top Level Domain:</span> .us</p>
+                        <p><span className="highlight">Currencies:</span> USD</p>
+                        <p><span className="highlight">Languages:</span> en</p>
+                    </section>
+                </div>
+
+                <div className="borders">
+                    <section>
+                        <h3>Border Countries:</h3>
+                        <!-- List of buttons -->
+                        <button>Mexico</button>
+                    </section>
+                </div>
             </div>
         </div>
     </div>
